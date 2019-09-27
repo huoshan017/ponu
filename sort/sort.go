540c6adb2f4ca88a7bf1e19c52f23fecf64f2f7f
huoshan017 <huoshan017@github.com>
2019-09-27 15:33:05 +0800

    11111

--- sort/sort.go
+++ sort/sort.go
@@ -22,16 +22,16 @@ func greater_equal(e1, e2 Elem) bool {
 }
 
 // ======================== quick sort =========================
-func qsort_partition(arr []Elem, low, high int) int {
+func qsort_partition(arr []Elem, low, high int, desc bool) int {
 	pivot := arr[low]
 	for low < high {
-		for low < high && less_equal(pivot, arr[high]) {
+		for low < high && ((!desc && less_equal(pivot, arr[high])) || (desc && less_equal(arr[high], pivot))) {
 			high -= 1
 		}
 		if low < high {
 			arr[low] = arr[high]
 		}
-		for low < high && less_equal(arr[low], pivot) {
+		for low < high && ((!desc && less_equal(arr[low], pivot)) || (desc && less_equal(pivot, arr[low]))) {
 			low += 1
 		}
 		if low < high {
@@ -42,16 +42,16 @@ func qsort_partition(arr []Elem, low, high int) int {
 	return low
 }
 
-func qsort(arr []Elem, low, high int) {
+func qsort(arr []Elem, low, high int, desc bool) {
 	if low < high {
-		pivot := qsort_partition(arr, low, high)
-		qsort(arr, low, pivot-1)
-		qsort(arr, pivot+1, high)
+		pivot := qsort_partition(arr, low, high, desc)
+		qsort(arr, low, pivot-1, desc)
+		qsort(arr, pivot+1, high, desc)
 	}
 }
 
-func QSort(arr []Elem) {
-	qsort(arr, 0, len(arr)-1)
+func QSort(arr []Elem, desc bool) {
+	qsort(arr, 0, len(arr)-1, desc)
 }
 
 // ======================= heap sort ========================
@@ -107,28 +107,28 @@ func heap_adjust_min(arr []Elem, s, m int) {
 	}
 }
 
-func HeapSortMax(arr []Elem) {
-	for i := len(arr)/2 - 1; i >= 0; i-- {
-		heap_adjust_max(arr, i, len(arr)-1)
-	}
-	for i := len(arr) - 1; i > 0; i-- {
-		arr[0], arr[i] = arr[i], arr[0]
-		heap_adjust_max(arr, 0, i-1)
-	}
-}
-
-func HeapSortMin(arr []Elem) {
-	for i := len(arr)/2 - 1; i >= 0; i-- {
-		heap_adjust_min(arr, i, len(arr)-1)
-	}
-	for i := len(arr) - 1; i > 0; i-- {
-		arr[0], arr[i] = arr[i], arr[0]
-		heap_adjust_min(arr, 0, i-1)
+func HeapSort(arr []Elem, desc bool) {
+	if !desc {
+		for i := len(arr)/2 - 1; i >= 0; i-- {
+			heap_adjust_max(arr, i, len(arr)-1)
+		}
+		for i := len(arr) - 1; i > 0; i-- {
+			arr[0], arr[i] = arr[i], arr[0]
+			heap_adjust_max(arr, 0, i-1)
+		}
+	} else {
+		for i := len(arr)/2 - 1; i >= 0; i-- {
+			heap_adjust_min(arr, i, len(arr)-1)
+		}
+		for i := len(arr) - 1; i > 0; i-- {
+			arr[0], arr[i] = arr[i], arr[0]
+			heap_adjust_min(arr, 0, i-1)
+		}
 	}
 }
 
 // ============================== merge sort =============================
-func MergeSort(arr []Elem) (out_arr []Elem) {
+func MergeSort(arr []Elem, desc bool) (out_arr []Elem) {
 	var arr2 []Elem
 	var l = len(arr)
 	if l > 2 {
@@ -138,13 +138,13 @@ func MergeSort(arr []Elem) (out_arr []Elem) {
 	var b bool
 	for s := 1; s <= l; s = s * 2 {
 		if s == 1 {
-			merge_first(arr)
+			merge_first(arr, desc)
 			out_arr = arr
 		} else {
 			if !b {
-				merge(arr, s, arr2)
+				merge(arr, s, arr2, desc)
 			} else {
-				merge(arr2, s, arr)
+				merge(arr2, s, arr, desc)
 			}
 			b = !b
 		}
@@ -158,19 +158,19 @@ func MergeSort(arr []Elem) (out_arr []Elem) {
 }
 
 // when step is 2
-func merge_first(arr []Elem) {
+func merge_first(arr []Elem, desc bool) {
 	l := len(arr)
 	for s := 0; s < l; s = s + 2 {
 		if s+1 >= l {
 			break
 		}
-		if arr[s+1].Less(arr[s]) {
+		if (!desc && arr[s+1].Less(arr[s])) || (desc && arr[s].Less(arr[s+1])) {
 			arr[s], arr[s+1] = arr[s+1], arr[s]
 		}
 	}
 }
 
-func merge(arr []Elem, step int, out_arr []Elem) {
+func merge(arr []Elem, step int, out_arr []Elem, desc bool) {
 	var (
 		s, i, j, n, imax, jmax int
 	)
@@ -190,7 +190,7 @@ func merge(arr []Elem, step int, out_arr []Elem) {
 			jmax = s + step + step
 		}
 		for i < imax && j < jmax {
-			if less_equal(arr[i], arr[j]) {
+			if (!desc && less_equal(arr[i], arr[j])) || (desc && less_equal(arr[j], arr[i])) {
 				out_arr[n] = arr[i]
 				i += 1
 			} else {
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -22,9 +22,31 @@ func (a Int) Equal(e Elem) bool {
 	return false
 }
 
+var test_arr = []Elem{Int(100), Int(90), Int(91), Int(1000), Int(-54), Int(2211), Int(1), Int(22), Int(411), Int(5), Int(333), Int(23421), Int(711), Int(6543), Int(869), Int(3), Int(108), Int(40), Int(64), Int(89), Int(77), Int(918), Int(101), Int(115), Int(609)}
+
 func Test_quicksort(t *testing.T) {
-	arr := []Elem{Int(100), Int(90), Int(91), Int(1000), Int(-54), Int(2211), Int(1), Int(22), Int(411), Int(5), Int(333), Int(23421), Int(711), Int(6543), Int(869), Int(3), Int(108), Int(40), Int(64), Int(89), Int(77), Int(918), Int(101), Int(115), Int(609)}
-	//HeapSortMin(arr)
-	arr2 := MergeSort(arr)
-	t.Logf("sorted: %v", arr2)
+	tmp := make([]Elem, len(test_arr))
+	for i := 0; i < 1000000; i++ {
+		copy(tmp, test_arr)
+		QSort(tmp, false)
+	}
+	t.Logf("quick sorted: %v", tmp)
+}
+
+func Test_heapsort(t *testing.T) {
+	tmp := make([]Elem, len(test_arr))
+	for i := 0; i < 1000000; i++ {
+		copy(tmp, test_arr)
+		HeapSort(tmp, false)
+	}
+	t.Logf("heap sorted: %v", tmp)
+}
+
+func Test_mergesort(t *testing.T) {
+	tmp := make([]Elem, len(test_arr))
+	for i := 0; i < 1000000; i++ {
+		copy(tmp, test_arr)
+		tmp = MergeSort(tmp, false)
+	}
+	t.Logf("merge sorted: %v", tmp)
 }
